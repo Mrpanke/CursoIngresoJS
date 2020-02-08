1725bc2cdc9c86d79af5e2c3fd04117ba5a89520
Facundo Acosta <60562945+Mrpanke@users.noreply.github.com>
2020-02-08 01:06:29 -0300

    Add files via upload
    Actualizacion IF

---
+++ jsInstruccionIF (5).js
@@ -0,0 +1,16 @@
+function mostrar()
+{
+
+var edad;
+
+edad = parseInt(document.getElementById("edad").value);
+
+if((edad < 13) && (edad > 17))
+{
+
+    alert("Usted no es adolecente");
+
+}
+
+
+}// preguntar bien
